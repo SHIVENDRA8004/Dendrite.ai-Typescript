906ceeb15e43784444ee809554da08b0f31d184a
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-19 01:07:36 +0530

    Created basic folders and all required components

---
+++ src/compoents/layouts/MusicGrid.tsx
@@ -0,0 +1,9 @@
+const MusicGrid = () => {
+  return (
+    <div className="musicGrid">
+      <h2> Music Play,Pause,Next,Previous,Volumn Rocker </h2>
+    </div>
+  );
+};
+
+export default MusicGrid;
